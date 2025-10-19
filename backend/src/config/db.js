6c353beda5d425a7810784eddb23e45ababd5cab
cauyatech <josselino.silva-da-costa@epitech.eu>
2025-10-19 16:58:53 +0200

    [+] Add Prisma schema and database configuration files

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** Epikodi [WSL: Ubuntu]
+** File description:
+** db
+*/
+
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
